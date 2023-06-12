8e72c8eb923b96e27b39e34c447f5c85d3a222d4
Faisal <faisalazam_1616@hotmail.com>
2023-06-12 12:39:33 +0500

    Initialised a new project to created CRUD functionality with JWT security.

---
+++ src/main/java/com/lucidtech/usercrudwithjwt/UserCRUDWithJWTApplication.java
@@ -0,0 +1,11 @@
+package com.lucidtech.usercrudwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCRUDWithJWTApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserCRUDWithJWTApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lucidtech/usercrudwithjwt/UsercrudwithjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucidtech.usercrudwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercrudwithjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
