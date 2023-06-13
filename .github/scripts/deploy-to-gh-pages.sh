b21ef360f26686fe7eb626088788bf0b86b43a07
Muhammad Faisal <faisalazam_1616@hotmail.com>
2023-06-14 00:54:18 +0500

    ci(Building and testing the app on GitHub): Extracted the shell script to a shell file.

---
+++ .github/scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git remote set-url origin https://git:"${GITHUB_TOKEN}"@github.com/"${GITHUB_REPOSITORY}".git
+mkdir /tmp/gh-pages
+cp README.md /tmp/gh-pages
+cp -r target/site /tmp/gh-pages/site
+cp target/*-SNAPSHOT*.jar* /tmp/gh-pages
+npx gh-pages --dist /tmp/gh-pages -u "github-actions-bot <support+actions@github.com>"
+rm -rf /tmp/gh-pages
