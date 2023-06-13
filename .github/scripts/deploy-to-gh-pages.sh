934b9f0dd440201f1db53210365957c063262a94
Muhammad Faisal <faisalazam_1616@hotmail.com>
2023-06-14 01:30:35 +0500

    ci(Building and testing the app on GitHub): Extracted the shell script to a shell file.

--- .github/scripts/deploy-to-gh-pages.sh
+++ .github/scripts/deploy-to-gh-pages.sh
@@ -4,6 +4,7 @@ git remote set-url origin https://git:"${GITHUB_TOKEN}"@github.com/"${GITHUB_REP
 mkdir /tmp/gh-pages
 cp README.md /tmp/gh-pages
 cp -r target/site /tmp/gh-pages/site
+cp -r target/surefire-reports /tmp/gh-pages/surefire-reports
 cp target/*-SNAPSHOT*.jar* /tmp/gh-pages
 npx gh-pages --dist /tmp/gh-pages -u "github-actions-bot <support+actions@github.com>"
 rm -rf /tmp/gh-pages
