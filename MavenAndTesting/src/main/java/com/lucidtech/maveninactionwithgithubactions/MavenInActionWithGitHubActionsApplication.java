6b19dd7708168095b156fbcba4d17100284aa8d4
Muhammad Faisal <faisalazam_1616@hotmail.com>
2023-06-20 01:46:23 +0500

    Added few java classes along with some sample unit as well as integration tests, and couple of spring boot dependencies, so that we can compile the code and run the tests.

---
+++ MavenAndTesting/src/main/java/com/lucidtech/maveninactionwithgithubactions/MavenInActionWithGitHubActionsApplication.java
@@ -0,0 +1,11 @@
+package com.lucidtech.maveninactionwithgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenInActionWithGitHubActionsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MavenInActionWithGitHubActionsApplication.class, args);
+    }
+}
---
+++ MavenAndTesting/src/test/java/com/lucidtech/maveninactionwithgithubactions/UserCRUDWithJWTApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lucidtech.maveninactionwithgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenInActionWithGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void applicationStarts() {
+        MavenInActionWithGitHubActionsApplication.main(new String[]{});
+    }
+}
